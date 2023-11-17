e525ffd438493bdcd700032ae2b758dc405c3426
Reut Vladislav <reut-2011@mail.ru>
2023-11-18 00:31:40 +0300

    feat: add getFibonacciNumber

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -258,8 +258,19 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  if (index <= 1) {
+    return index;
+  }
+  let firstNum = 0;
+  let secondNum = 1;
+  let numberFibo;
+  for (let i = 2; i <= index; i += 1) {
+    numberFibo = firstNum + secondNum;
+    firstNum = secondNum;
+    secondNum = numberFibo;
+  }
+  return secondNum;
 }
 
 /**
